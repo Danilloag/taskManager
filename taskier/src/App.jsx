32024b8bd3117ce19c73a42db07bee3e75d85121
Cauê Coser <cauecoser@gmail.com>
2023-06-28 20:09:55 -0300

    Criação e organização do componente que comporta colunas de tarefas.

--- taskier/src/App.jsx
+++ taskier/src/App.jsx
@@ -2,30 +2,31 @@ import './App.css'
 import './grid.css'
 // import Task from './components/task/task'
 import LogoMach1 from './assets/images/mach-1-logo.png'
+import Column from './components/column/column'
 
 function App() {
 
   return (
-    <div className='background grid'>
-      <div className='logo-div col-12'>
-        <img className='logo-mach' src={LogoMach1} alt="LogoMach1" />
-      </div>
-      <div className='bread-div col-12'>
-        <p className='breadcrumbs'>BREADCRUMBS</p>
-      </div>
-
-      {/* master */}
-      {/* manager --> busca, colunas, tasks*/}
-      {/* include form */}
-      {/* edit form */}
-      {/* /master */}
-
-
-
-
-
-
-      {/* <Task></Task> */}
+    <div className='background'>
+        <div className='logo-div col-12'>
+          <img className='logo-mach' src={LogoMach1} alt="LogoMach1" />
+        </div>
+        <div className='bread-div col-12'>
+          <p className='breadcrumbs'>BREADCRUMBS</p>
+        </div>
+     
+     
+        <div className='master-div grid'>
+          <div className='column-container col-4'>
+            <Column></Column>
+          </div>
+          <div className='column-container col-4'>
+            <Column></Column>
+          </div>
+          <div className='column-container col-4'>
+            <Column></Column>
+          </div>
+        </div>
 
     </div>
   )
