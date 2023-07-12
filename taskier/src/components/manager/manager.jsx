a4b3b3f7371b95c478d1e2987b7d6c7691dcf7bd
Cauê Coser <cauecoser@gmail.com>
2023-07-12 20:08:39 -0300

    Restauração do mecanismo de busca.

--- taskier/src/components/manager/manager.jsx
+++ taskier/src/components/manager/manager.jsx
@@ -3,33 +3,37 @@ import Column from '../column/Column';
 import { useEffect, useContext } from 'react';
 import tasksContext from '../../tasksContext';
 
+
 export default function Manager() {
-  const { tasks, loadTasks } = useContext(tasksContext);
+  const { tasks, setTasks, loadTasks } = useContext(tasksContext);
 
-  useEffect(() => {
-    loadTasks();
-  }, []);
+    useEffect(() => {
+        loadTasks()
+    }, [])
+    
+    async function filterTasks(e) {
+        if (e.target.value != '') {
+            const tasksToShow = tasks.map((task) => ({...task, show: task.name.toLowerCase().includes(e.target.value.toLowerCase())}))
+            setTasks(tasksToShow)
+        }
+    }
 
-  return (
-    <div className="master-div">
-      <div className="search-div grid">
-        <input
-          className="home-search col-12"
-          type="search"
-          placeholder="Search a title..."
-        />
-      </div>
-      <div className="columns-div grid">
-        <div className="column-container col-4">
-          <Column data={tasks} status="LISTADA" title="Listed" />
+    return (
+        <div className='master-div'>
+            <div className='search-div grid'>
+                <input onChange={(e) => filterTasks(e)} className='home-search col-12' type="search" placeholder='Search a title...' />
+            </div>
+            <div className='columns-div grid'>
+                <div className='column-container col-4'>
+                    <Column data={tasks} status="LISTADA" title='Listed'></Column>
+                </div>
+                <div className='column-container col-4'>
+                    <Column data={tasks} status="INICIADA" title='Started'></Column>
+                </div>
+                <div className='column-container col-4'>
+                    <Column data={tasks} status="FINALIZADA" title='Ended'></Column>
+                </div>
+            </div>
         </div>
-        <div className="column-container col-4">
-          <Column data={tasks} status="INICIADA" title="Started" />
-        </div>
-        <div className="column-container col-4">
-          <Column data={tasks} status="FINALIZADA" title="Ended" />
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
